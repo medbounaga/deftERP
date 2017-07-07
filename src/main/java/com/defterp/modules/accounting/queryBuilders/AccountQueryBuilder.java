22b2d1bab9a890ea6ac5477582e303226ab1d4e2
Mohammed Bounaga <medbounaga@gmail.com>
2017-07-07 01:41:04 +0100

    AccountQueryBuilder cleanup

--- src/main/java/com/defterp/modules/accounting/queryBuilders/AccountQueryBuilder.java
+++ src/main/java/com/defterp/modules/accounting/queryBuilders/AccountQueryBuilder.java
@@ -3,30 +3,27 @@ package com.defterp.modules.accounting.queryBuilders;
 import com.defterp.util.QueryWrapper;
 
 
-public class AccountQueryBuilder
-{
+public class AccountQueryBuilder{
+    
   private static final String FIND_BY_NAME = "SELECT a FROM Account a WHERE a.title = :name";
   private static final String FIND_BY_TYPE = "SELECT a FROM Account a WHERE a.type = :type ";
   private static final String FIND_ALL_ACCOUNTS = "SELECT a FROM Account a";
   
-  public static QueryWrapper getFindByNameQuery(String accountName)
-  {
+  public static QueryWrapper getFindByNameQuery(String accountName){
     QueryWrapper qw = new QueryWrapper(FIND_BY_NAME)
                           .setParameter("name", accountName);
     
     return qw;
   }
   
-  public static QueryWrapper getFindByTypeQuery(String accountType)
-  {
+  public static QueryWrapper getFindByTypeQuery(String accountType){
     QueryWrapper qw = new QueryWrapper(FIND_BY_TYPE)
                           .setParameter("type", accountType);
     
     return qw;
   }
   
-  public static QueryWrapper getFindAllAccountsQuery()
-  {
+  public static QueryWrapper getFindAllAccountsQuery(){
     QueryWrapper qw = new QueryWrapper(FIND_ALL_ACCOUNTS);
     
     return qw;
