596e446ce2f1ad4d3057ca7d1eb88b908822c50a
Mohammed Bounaga <medbounaga@gmail.com>
2017-07-07 23:52:26 +0100

    JournalItemQueryBuilder code refactoring

--- src/main/java/com/defterp/modules/accounting/queryBuilders/JournalItemQueryBuilder.java
+++ src/main/java/com/defterp/modules/accounting/queryBuilders/JournalItemQueryBuilder.java
@@ -2,29 +2,20 @@ package com.defterp.modules.accounting.queryBuilders;
 
 import com.defterp.modules.commonClasses.QueryWrapper;
 
+public class JournalItemQueryBuilder {
 
-public class JournalItemQueryBuilder
-{
-  private static final String FIND_CREDIT_SUM_BY_ACCOUNT = "SELECT SUM(j.credit) FROM JournalItem j WHERE j.account.id = :accountId ";
-  private static final String FIND_DEBIT_SUM_BY_ACCOUNT = "SELECT SUM(j.debit) FROM JournalItem j WHERE j.account.id = :accountId";
-  
-  public static QueryWrapper getFindCreditSumByAccountQuery(Integer accountId)
-  {
-    QueryWrapper qw = new QueryWrapper("SELECT SUM(j.credit) FROM JournalItem j WHERE j.account.id = :accountId ").setParameter("accountId", accountId);
-    
-    return qw;
-  }
-  
-  public static QueryWrapper getFindDebitSumByAccountQuery(Integer accountId)
-  {
-    QueryWrapper qw = new QueryWrapper("SELECT SUM(j.debit) FROM JournalItem j WHERE j.account.id = :accountId").setParameter("accountId", accountId);
-    
-    return qw;
-  }
+    private static final String FIND_CREDIT_SUM_BY_ACCOUNT = "SELECT SUM(j.credit) FROM JournalItem j WHERE j.account.id = :accountId ";
+    private static final String FIND_DEBIT_SUM_BY_ACCOUNT = "SELECT SUM(j.debit) FROM JournalItem j WHERE j.account.id = :accountId";
+
+    public static QueryWrapper getFindCreditSumByAccountQuery(Integer accountId) {
+
+        return new QueryWrapper(FIND_CREDIT_SUM_BY_ACCOUNT)
+                   .setParameter("accountId", accountId);
+    }
+
+    public static QueryWrapper getFindDebitSumByAccountQuery(Integer accountId) {
+
+        return new QueryWrapper(FIND_DEBIT_SUM_BY_ACCOUNT)
+                   .setParameter("accountId", accountId);
+    }
 }
-
-/* Location:
- * Qualified Name:     JournalItemQueryBuilder
- * Java Class Version: 8 (52.0)
- * JD-Core Version:    0.7.1
- */
