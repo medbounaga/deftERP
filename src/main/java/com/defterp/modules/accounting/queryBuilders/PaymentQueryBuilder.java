5b4a7ad852f7d7180b2ccb581fdd063109bd49b9
Mohammed Bounaga <medbounaga@gmail.com>
2017-07-08 00:51:20 +0100

    Add new Query generator methods in "PaymentQueryBuilder"

--- src/main/java/com/defterp/modules/accounting/queryBuilders/PaymentQueryBuilder.java
+++ src/main/java/com/defterp/modules/accounting/queryBuilders/PaymentQueryBuilder.java
@@ -2,29 +2,66 @@ package com.defterp.modules.accounting.queryBuilders;
 
 import com.defterp.modules.commonClasses.QueryWrapper;
 
+public class PaymentQueryBuilder {
 
-public class PaymentQueryBuilder
-{
-  private static final String FIND_OUTSTANDING_BY_CUSTOMER = "SELECT p FROM Payment p WHERE p.partner.id = :partnerId AND p.type = 'in' AND p.state = 'Posted' AND p.partnerType = 'customer' AND p.overpayment > 0";
-  private static final String FIND_OUTSTANDING_BY_VENDOR = "SELECT p FROM Payment p WHERE p.partner.id = :partnerId AND p.type = 'out' AND p.state = 'Posted' AND p.partnerType = 'supplier' AND p.overpayment > 0";
-  
-  public static QueryWrapper getFindOutstandingByCustomer(Integer partnerId)
-  {
-    QueryWrapper qw = new QueryWrapper("SELECT p FROM Payment p WHERE p.partner.id = :partnerId AND p.type = 'in' AND p.state = 'Posted' AND p.partnerType = 'customer' AND p.overpayment > 0").setParameter("partnerId", partnerId);
-    
-    return qw;
-  }
-  
-  public static QueryWrapper getFindOutstandingByVendor(Integer partnerId)
-  {
-    QueryWrapper qw = new QueryWrapper("SELECT p FROM Payment p WHERE p.partner.id = :partnerId AND p.type = 'out' AND p.state = 'Posted' AND p.partnerType = 'supplier' AND p.overpayment > 0").setParameter("partnerId", partnerId);
-    
-    return qw;
-  }
+    private static final String FIND_BY_PARTNER_TYPE = "SELECT p FROM Payment p WHERE p.partnerType = :partnerType ";
+    private static final String FIND_BY_PARTNER = "SELECT p FROM Payment p WHERE p.partnerType = :partnerType AND p.partner.id = :partnerId";
+    private static final String FIND_OUTSTANDING_BY_PARTNER = "SELECT p FROM Payment p WHERE p.partner.id = :partnerId AND p.type = :paymentType AND p.state = 'Posted' AND p.partnerType = :partnerType AND p.overpayment > 0";
+    private static final String COUNT_BY_PARTNER = "SELECT COUNT(p) FROM Payment p WHERE p.partner.id = :partnerId AND p.partnerType = :partnerType ";
+
+    public static QueryWrapper getFindAllCustomerPaymentsQuery() {
+
+        return new QueryWrapper(FIND_BY_PARTNER_TYPE)
+                .setParameter("partnerType", "customer");
+    }
+
+    public static QueryWrapper getFindAllVendorPaymentsQuery() {
+
+        return new QueryWrapper(FIND_BY_PARTNER_TYPE)
+                .setParameter("partnerType", "supplier");
+    }
+
+    public static QueryWrapper getCountByVendorQuery(Integer vendorId) {
+
+        return new QueryWrapper(COUNT_BY_PARTNER)
+                .setParameter("partnerId", vendorId)
+                .setParameter("partnerType", "supplier");
+    }
+
+    public static QueryWrapper getCountByCustomerQuery(Integer customerId) {
+
+        return new QueryWrapper(COUNT_BY_PARTNER)
+                .setParameter("partnerId", customerId)
+                .setParameter("partnerType", "customer");
+    }
+
+    public static QueryWrapper getFindByCustomerQuery(Integer customerId) {
+
+        return new QueryWrapper(FIND_BY_PARTNER)
+                .setParameter("partnerId", customerId)
+                .setParameter("partnerType", "customer");
+    }
+
+    public static QueryWrapper getFindByVendorQuery(Integer vendorId) {
+
+        return new QueryWrapper(FIND_BY_PARTNER)
+                .setParameter("partnerId", vendorId)
+                .setParameter("partnerType", "supplier");
+    }
+
+    public static QueryWrapper getFindOutstandingByCustomerQuery(Integer partnerId) {
+
+        return new QueryWrapper(FIND_OUTSTANDING_BY_PARTNER)
+                .setParameter("partnerId", partnerId)
+                .setParameter("partnerType", "customer")
+                .setParameter("paymentType", "in");
+    }
+
+    public static QueryWrapper getFindOutstandingByVendorQuery(Integer partnerId) {
+
+        return new QueryWrapper(FIND_OUTSTANDING_BY_PARTNER)
+                .setParameter("partnerId", partnerId)
+                .setParameter("partnerType", "supplier")
+                .setParameter("paymentType", "out");
+    }
 }
-
-/* Location:
- * Qualified Name:     PaymentQueryBuilder
- * Java Class Version: 8 (52.0)
- * JD-Core Version:    0.7.1
- */
