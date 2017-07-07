9d921720714c437c6f46c37111a4e40c9de0a952
Mohammed Bounaga <medbounaga@gmail.com>
2017-07-07 02:22:27 +0100

    create "UserQueryBuilder" under "modules.users.queryBuilders" package

---
+++ src/main/java/com/defterp/modules/users/queryBuilders/UserQueryBuilder.java
@@ -0,0 +1,25 @@
+package com.defterp.modules.users.queryBuilders;
+
+import com.defterp.util.QueryWrapper;
+
+public class UserQueryBuilder {
+
+    private static final String FIND_ALL = "SELECT u FROM User u";
+    private static final String FIND_BY_LOGIN = "SELECT u FROM User u WHERE u.login = :login AND u.password = :password AND u.active = true";
+
+    private static QueryWrapper query;
+
+    public static QueryWrapper getFindAllQuery() {
+        query = new QueryWrapper(FIND_ALL);
+
+        return query;
+    }
+
+    public static QueryWrapper getFindByLoginQuery(String userName, String password) {
+        query = new QueryWrapper(FIND_BY_LOGIN)
+                .setParameter("login", userName)
+                .setParameter("password", password);
+
+        return query;
+    }
+}
