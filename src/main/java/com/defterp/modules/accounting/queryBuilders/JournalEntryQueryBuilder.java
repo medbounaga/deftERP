0e4d840d944dad9edda6b80c8bda823a894d032b
Mohammed Bounaga <medbounaga@gmail.com>
2017-07-07 23:46:33 +0100

    Create JournalEntryQueryBuilder under "accounting.queryBuilders" package

---
+++ src/main/java/com/defterp/modules/accounting/queryBuilders/JournalEntryQueryBuilder.java
@@ -0,0 +1,14 @@
+package com.defterp.modules.accounting.queryBuilders;
+
+import com.defterp.modules.commonClasses.QueryWrapper;
+
+public class JournalEntryQueryBuilder {
+
+    private static final String FIND_ALL = "SELECT j FROM JournalEntry j";
+
+    public static QueryWrapper getFindAllJournalEntriesQuery() {
+
+        return new QueryWrapper(FIND_ALL);
+    }
+
+}
