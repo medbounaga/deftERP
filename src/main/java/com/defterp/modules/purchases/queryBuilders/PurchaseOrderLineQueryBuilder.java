30fc7ea0866ac19422df449ad3f14b92cc0d10f6
Mohammed Bounaga <medbounaga@gmail.com>
2017-07-08 04:32:29 +0100

    created "PurchaseOrderLineQueryBuilder" class  under "purchases.queryBuilders" package

---
+++ src/main/java/com/defterp/modules/purchases/queryBuilders/PurchaseOrderLineQueryBuilder.java
@@ -0,0 +1,22 @@
+package com.defterp.modules.purchases.queryBuilders;
+
+import com.defterp.modules.commonClasses.QueryWrapper;
+
+public class PurchaseOrderLineQueryBuilder {
+
+    private static final String FIND_BY_PRODUCT = "SELECT p FROM PurchaseOrderLine p WHERE p.product.id = :productId";
+    private static final String TOTAL_PRODUCT_QUANTITY = "SELECT SUM(p.quantity) FROM PurchaseOrderLine p WHERE p.product.id = :productId ";
+
+    public static QueryWrapper getFindByProductQuery(Integer productId) {
+
+        return new QueryWrapper(FIND_BY_PRODUCT)
+                .setParameter("productId", productId);
+    }
+
+    public static QueryWrapper getCountQuantityByProductQuery(Integer productId) {
+
+        return new QueryWrapper(TOTAL_PRODUCT_QUANTITY)
+                .setParameter("productId", productId);
+    }
+
+}
