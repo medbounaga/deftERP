bb166fc6f33ebeb0df0713fa75f4a2c370ec8798
Mohammed Bounaga <medbounaga@gmail.com>
2017-07-08 03:04:02 +0100

    "ProductQueryBuilder" - code refactoring

--- src/main/java/com/defterp/modules/inventory/queryBuilders/ProductQueryBuilder.java
+++ src/main/java/com/defterp/modules/inventory/queryBuilders/ProductQueryBuilder.java
@@ -2,52 +2,36 @@ package com.defterp.modules.inventory.queryBuilders;
 
 import com.defterp.modules.commonClasses.QueryWrapper;
 
-public class ProductQueryBuilder
-{
-  private static final String FIND_ALL_PRODUCTS = "SELECT p FROM Product p";
-  private static final String FIND_SOLD_PRODUCTS = "SELECT p FROM Product p WHERE p.saleOk = true";
-  private static final String FIND_ACTIVE_SOLD_PRODUCTS = "SELECT p FROM Product p WHERE p.saleOk = true AND p.active = true";
-  private static final String FIND_PURCHASED_PRODUCTS = "SELECT p FROM Product p WHERE p.purchaseOk = true";
-  private static final String FIND_ACTIVE_PURCHASED_PRODUCTS = "SELECT p FROM Product p WHERE p.purchaseOk = true AND p.active = true";
-  
-  public static QueryWrapper getFindAllProductsQuery()
-  {
-    QueryWrapper query = new QueryWrapper("SELECT p FROM Product p");
-    
-    return query;
-  }
-  
-  public static QueryWrapper getFindSoldProductsQuery()
-  {
-    QueryWrapper query = new QueryWrapper("SELECT p FROM Product p WHERE p.saleOk = true");
-    
-    return query;
-  }
-  
-  public static QueryWrapper getFindActiveSoldProductsQuery()
-  {
-    QueryWrapper query = new QueryWrapper("SELECT p FROM Product p WHERE p.saleOk = true AND p.active = true");
-    
-    return query;
-  }
-  
-  public static QueryWrapper getFindPurchasedProductsQuery()
-  {
-    QueryWrapper query = new QueryWrapper("SELECT p FROM Product p WHERE p.purchaseOk = true");
-    
-    return query;
-  }
-  
-  public static QueryWrapper getFindActivePurchasedProductsQuery()
-  {
-    QueryWrapper query = new QueryWrapper("SELECT p FROM Product p WHERE p.purchaseOk = true AND p.active = true");
-    
-    return query;
-  }
-}
+public class ProductQueryBuilder {
 
-/* Location:
- * Qualified Name:     ProductQueryBuilder
- * Java Class Version: 8 (52.0)
- * JD-Core Version:    0.7.1
- */
+    private static final String FIND_ALL = "SELECT p FROM Product p";
+    private static final String FIND_SOLD_PRODUCTS = "SELECT p FROM Product p WHERE p.saleOk = true";
+    private static final String FIND_ACTIVE_SOLD_PRODUCTS = "SELECT p FROM Product p WHERE p.saleOk = true AND p.active = true";
+    private static final String FIND_PURCHASED_PRODUCTS = "SELECT p FROM Product p WHERE p.purchaseOk = true";
+    private static final String FIND_ACTIVE_PURCHASED_PRODUCTS = "SELECT p FROM Product p WHERE p.purchaseOk = true AND p.active = true";
+
+    public static QueryWrapper getFindAllQuery() {
+
+        return new QueryWrapper(FIND_ALL);
+    }
+
+    public static QueryWrapper getFindSoldProductsQuery() {
+
+        return new QueryWrapper(FIND_SOLD_PRODUCTS);
+    }
+
+    public static QueryWrapper getFindActiveSoldProductsQuery() {
+
+        return new QueryWrapper(FIND_ACTIVE_SOLD_PRODUCTS);
+    }
+
+    public static QueryWrapper getFindPurchasedProductsQuery() {
+
+        return new QueryWrapper(FIND_PURCHASED_PRODUCTS);
+    }
+
+    public static QueryWrapper getFindActivePurchasedProductsQuery() {
+
+        return new QueryWrapper(FIND_ACTIVE_PURCHASED_PRODUCTS);
+    }
+}
