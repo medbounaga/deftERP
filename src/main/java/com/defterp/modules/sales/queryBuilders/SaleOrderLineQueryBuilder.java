0dc0a177111510ce213387e4d57d60c26e9e49c1
Mohammed Bounaga <medbounaga@gmail.com>
2017-07-08 04:52:21 +0100

    Created "SaleOrderLineQueryBuilder" class under "modules.sales.queryBuilders" package

---
+++ src/main/java/com/defterp/modules/sales/queryBuilders/SaleOrderLineQueryBuilder.java
@@ -0,0 +1,22 @@
+package com.defterp.modules.sales.queryBuilders;
+
+import com.defterp.modules.commonClasses.QueryWrapper;
+
+public class SaleOrderLineQueryBuilder {
+
+    private static final String FIND_BY_PRODUCT = "SELECT s FROM SaleOrderLine s WHERE s.product.id = :productId";
+    private static final String TOTAL_PRODUCT_QUANTITY = "SELECT SUM(s.quantity) FROM SaleOrderLine s WHERE s.product.id = :productId ";
+
+    public static QueryWrapper getFindByProductQuery(Integer productId) {
+
+        return new QueryWrapper(FIND_BY_PRODUCT)
+                .setParameter("productId", productId);
+    }
+
+    public static QueryWrapper getTotalProductQuantityQuery(Integer productId) {
+
+        return new QueryWrapper(TOTAL_PRODUCT_QUANTITY)
+                .setParameter("productId", productId);
+    }
+
+}
