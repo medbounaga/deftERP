310c6f030fb88028bae1efcd2cfee22ff6a90b82
Mohammed Bounaga <medbounaga@gmail.com>
2017-07-09 06:54:04 +0100

    Replaced "JournalDAO" with "GenericDAO" in JournalConverter class

--- src/main/java/com/defterp/modules/accounting/converters/JournalConverter.java
+++ src/main/java/com/defterp/modules/accounting/converters/JournalConverter.java
@@ -1,7 +1,7 @@
 package com.defterp.modules.accounting.converters;
 
 import com.defterp.modules.accounting.entities.Journal;
-import com.casa.erp.dao.JournalFacade;
+import com.defterp.dataAccess.GenericDAO;
 import com.defterp.util.JsfUtil;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -22,23 +22,23 @@ import javax.faces.convert.FacesConverter;
 public class JournalConverter implements Converter {
 
     @Inject
-    private JournalFacade ejbFacade;
+    private GenericDAO dataAccess;
 
     @Override
     public Object getAsObject(FacesContext facesContext, UIComponent component, String value) {
         if (value == null || value.length() == 0 || JsfUtil.isDummySelectItem(component, value)) {
             return null;
         }
-        return this.ejbFacade.find(getKey(value));
+        return dataAccess.findById(getKey(value), Journal.class);
     }
 
-    java.lang.Integer getKey(String value) {
-        java.lang.Integer key;
+    private Integer getKey(String value) {
+        Integer key;
         key = Integer.valueOf(value);
         return key;
     }
 
-    String getStringKey(java.lang.Integer value) {
+    private String getStringKey(Integer value) {
         StringBuffer sb = new StringBuffer();
         sb.append(value);
         return sb.toString();
