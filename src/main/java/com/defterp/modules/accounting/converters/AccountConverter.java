7072c975d6f418743b557d75e6df01a1556c7d73
Mohammed Bounaga <medbounaga@gmail.com>
2017-07-09 06:40:03 +0100

    Replaced "AccountDAO" with "GenericDAO"  in AccountConverter class

--- src/main/java/com/defterp/modules/accounting/converters/AccountConverter.java
+++ src/main/java/com/defterp/modules/accounting/converters/AccountConverter.java
@@ -1,7 +1,7 @@
 package com.defterp.modules.accounting.converters;
 
 import com.defterp.modules.accounting.entities.Account;
-import com.casa.erp.dao.AccountFacade;
+import com.defterp.dataAccess.GenericDAO;
 import com.defterp.util.JsfUtil;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -22,23 +22,23 @@ import javax.faces.convert.FacesConverter;
 public class AccountConverter implements Converter {
 
     @Inject
-    private AccountFacade ejbFacade;
+    private GenericDAO dataAccess;
 
     @Override
     public Object getAsObject(FacesContext facesContext, UIComponent component, String value) {
         if (value == null || value.length() == 0 || JsfUtil.isDummySelectItem(component, value)) {
             return null;
         }
-        return this.ejbFacade.find(getKey(value));
+        return dataAccess.findById(getKey(value), Account.class);
     }
 
-    java.lang.Integer getKey(String value) {
-        java.lang.Integer key;
+    private Integer getKey(String value) {
+        Integer key;
         key = Integer.valueOf(value);
         return key;
     }
 
-    String getStringKey(java.lang.Integer value) {
+    private String getStringKey(Integer value) {
         StringBuffer sb = new StringBuffer();
         sb.append(value);
         return sb.toString();
@@ -46,8 +46,7 @@ public class AccountConverter implements Converter {
 
     @Override
     public String getAsString(FacesContext facesContext, UIComponent component, Object object) {
-        if (object == null
-                || (object instanceof String && ((String) object).length() == 0)) {
+        if (object == null || (object instanceof String && ((String) object).length() == 0)) {
             return null;
         }
         if (object instanceof Account) {
