150c67eea0a33c8fe76fd90b5c44ba91efd610f3
Mohammed Bounaga <medbounaga@gmail.com>
2017-07-10 02:22:38 +0100

    Replaced "PaymentDAO" with "GenericDAO" in "PaymenConverter" class

--- src/main/java/com/defterp/modules/accounting/converters/PaymentConverter.java
+++ src/main/java/com/defterp/modules/accounting/converters/PaymentConverter.java
@@ -1,7 +1,7 @@
 package com.defterp.modules.accounting.converters;
 
 import com.defterp.modules.accounting.entities.Payment;
-import com.casa.erp.dao.PaymentFacade;
+import com.defterp.dataAccess.GenericDAO;
 import com.defterp.util.JsfUtil;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -12,9 +12,9 @@ import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
 
 /**
- * 
+ *
  * @author MOHAMMED BOUNAGA
- * 
+ *
  * github.com/medbounaga
  */
 
@@ -22,23 +22,23 @@ import javax.faces.convert.FacesConverter;
 public class PaymentConverter implements Converter {
 
     @Inject
-    private PaymentFacade ejbFacade;
+    private GenericDAO dataAccess;
 
     @Override
     public Object getAsObject(FacesContext facesContext, UIComponent component, String value) {
         if (value == null || value.length() == 0 || JsfUtil.isDummySelectItem(component, value)) {
             return null;
         }
-        return this.ejbFacade.find(getKey(value));
+        return dataAccess.findById(getKey(value), Payment.class);
     }
 
-    java.lang.Integer getKey(String value) {
-        java.lang.Integer key;
+    private Integer getKey(String value) {
+        Integer key;
         key = Integer.valueOf(value);
         return key;
     }
 
-    String getStringKey(java.lang.Integer value) {
+    private String getStringKey(Integer value) {
         StringBuffer sb = new StringBuffer();
         sb.append(value);
         return sb.toString();
@@ -46,8 +46,7 @@ public class PaymentConverter implements Converter {
 
     @Override
     public String getAsString(FacesContext facesContext, UIComponent component, Object object) {
-        if (object == null
-                || (object instanceof String && ((String) object).length() == 0)) {
+        if (object == null || (object instanceof String && ((String) object).length() == 0)) {
             return null;
         }
         if (object instanceof Payment) {
