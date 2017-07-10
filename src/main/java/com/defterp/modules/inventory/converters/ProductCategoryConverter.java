f16001a18cb81af60ca60dc3cf64fbd9becd4b10
Mohammed Bounaga <medbounaga@gmail.com>
2017-07-10 08:54:12 +0100

    Replaced "ProductCategoryDAO" with "GenericDAO" in "ProductCategoryConverter" class

--- src/main/java/com/defterp/modules/inventory/converters/ProductCategoryConverter.java
+++ src/main/java/com/defterp/modules/inventory/converters/ProductCategoryConverter.java
@@ -1,7 +1,7 @@
 package com.defterp.modules.inventory.converters;
 
 import com.defterp.modules.inventory.entities.ProductCategory;
-import com.casa.erp.dao.ProductCategoryFacade;
+import com.defterp.dataAccess.GenericDAO;
 import com.defterp.util.JsfUtil;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -22,23 +22,23 @@ import javax.faces.convert.FacesConverter;
 public class ProductCategoryConverter implements Converter {
 
     @Inject
-    private ProductCategoryFacade ejbFacade;
+    private GenericDAO dataAccess;
 
     @Override
     public Object getAsObject(FacesContext facesContext, UIComponent component, String value) {
         if (value == null || value.length() == 0 || JsfUtil.isDummySelectItem(component, value)) {
             return null;
         }
-        return this.ejbFacade.find(getKey(value));
+        return dataAccess.findById(getKey(value), ProductCategory.class);
     }
 
-    java.lang.Integer getKey(String value) {
+    private Integer getKey(String value) {
         java.lang.Integer key;
         key = Integer.valueOf(value);
         return key;
     }
 
-    String getStringKey(java.lang.Integer value) {
+    private String getStringKey(Integer value) {
         StringBuffer sb = new StringBuffer();
         sb.append(value);
         return sb.toString();
@@ -46,8 +46,7 @@ public class ProductCategoryConverter implements Converter {
 
     @Override
     public String getAsString(FacesContext facesContext, UIComponent component, Object object) {
-        if (object == null
-                || (object instanceof String && ((String) object).length() == 0)) {
+        if (object == null || (object instanceof String && ((String) object).length() == 0)) {
             return null;
         }
         if (object instanceof ProductCategory) {
