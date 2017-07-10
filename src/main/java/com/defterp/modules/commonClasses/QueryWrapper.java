d7bffb100535943a62a09de3136508183a1a4e5a
Mohammed Bounaga <medbounaga@gmail.com>
2017-07-10 02:33:55 +0100

    .

--- src/main/java/com/defterp/modules/commonClasses/QueryWrapper.java
+++ src/main/java/com/defterp/modules/commonClasses/QueryWrapper.java
@@ -3,31 +3,26 @@ package com.defterp.modules.commonClasses;
 import java.util.HashMap;
 import java.util.Map;
 
-public class QueryWrapper
-{
-  private final String query;
-  private final Map<String, Object> parameters;
-  
-  public QueryWrapper(String query)
-  {
-    parameters = new HashMap();
-    this.query = query;
-  }
-  
-  public String getQuery()
-  {
-    return query;
-  }
-  
-  public Map<String, Object> getParameters()
-  {
-    return parameters;
-  }
-  
-  public QueryWrapper setParameter(String key, Object value)
-  {
-    parameters.put(key, value);
-    return this;
-  }
-}
+public class QueryWrapper {
 
+    private final String query;
+    private final Map<String, Object> parameters;
+
+    public QueryWrapper(String query) {
+        parameters = new HashMap();
+        this.query = query;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public Map<String, Object> getParameters() {
+        return parameters;
+    }
+
+    public QueryWrapper setParameter(String key, Object value) {
+        parameters.put(key, value);
+        return this;
+    }
+}
